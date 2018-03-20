e0093d667e853c1d5ddd9b1fb640fff5341ae840
Anthony Jackman <energticpixels@gmail.com>
2018-03-20 18:49:32 -0500

    first method

--- public/script.js
+++ public/script.js
@@ -4,5 +4,11 @@ new Vue({
     el: '#app',
     data: {
         total: 0
+    },
+    methods: {
+        addItem: function() {
+            //console.log('addItem')
+            this.total += 9.99;
+        }
     }
 })
