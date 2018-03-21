6b49b86f91fcb957e94f663605b0b73307425c12
Anthony Jackman <energticpixels@gmail.com>
2018-03-20 20:20:05 -0500

    addItem btn class fix, fixing for loop in addItem

--- public/script.js
+++ public/script.js
@@ -25,6 +25,7 @@ new Vue({
                 if(this.cart[i].id === item.id) {
                     found = true;
                     this.cart[i].qty++;
+                    break;
                 }
             }
             if (!found) {
