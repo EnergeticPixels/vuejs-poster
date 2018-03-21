2fc3c9b8fc3d2a419c1a8b25cd58ec2ea4c1dbdd
Anthony Jackman <energticpixels@gmail.com>
2018-03-20 19:50:19 -0500

    cart items qty logic

--- public/script.js
+++ public/script.js
@@ -5,9 +5,9 @@ new Vue({
     data: {
         total: 0,
         items: [
-            {title: 'Item 1'},
-            {title: 'Item 2'},
-            {title: 'Item 3'}
+            {id: 1, title: 'Item 1'},
+            {id: 2, title: 'Item 2'},
+            {id: 3, title: 'Item 3'}
         ],
         cart: [
 
@@ -19,10 +19,21 @@ new Vue({
             //console.log(index);
             this.total += 9.99;
             var item = this.items[index];
-            this.cart.push({
-                title: item.title,
-                qty: 1
-            });
+            var found = false;
+            for (var i =0; i < this.cart.length; i++) {
+                if(this.cart[i].id === item.id) {
+                    found = true;
+                    this.cart[i].qty++;
+                }
+            }
+            if (!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1
+                });
+            }
+            
         }
     }
 })
