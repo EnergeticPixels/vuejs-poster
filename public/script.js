4634bd6c0b8a0404cf18ee336732fa59881ab1dd
Anthony Jackman <energticpixels@gmail.com>
2018-03-20 19:17:18 -0500

    creating cart

--- public/script.js
+++ public/script.js
@@ -8,11 +8,15 @@ new Vue({
             {title: 'Item 1'},
             {title: 'Item 2'},
             {title: 'Item 3'}
+        ],
+        cart: [
+
         ]
     },
     methods: {
-        addItem: function() {
+        addItem: function(index) {
             //console.log('addItem')
+            console.log(index);
             this.total += 9.99;
         }
     }
