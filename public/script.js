40ce3f754cff215a6adaaf843f359a268ec6db4b
Anthony Jackman <energticpixels@gmail.com>
2018-03-21 20:23:49 -0500

    default search on page load

--- public/script.js
+++ public/script.js
@@ -7,7 +7,7 @@ new Vue({
         total: 0,
         items: [],
         cart: [],
-        newSearch: '',
+        newSearch: 'anime',
         lastSearch: '',
         loading: false
     },
@@ -68,5 +68,8 @@ new Vue({
         currency: function(price) {
             return '$ '.concat(price.toFixed(2));
         }
+    },
+    mounted: function() {
+        this.onSubmit();
     }
 })
